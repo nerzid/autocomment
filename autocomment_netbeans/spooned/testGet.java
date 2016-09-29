6f63b2f348b764d9822221cae92a65277cc19686
nerzid <nerzid@gmail.com>
2016-09-29 16:07:00 +0300

    Method to check whether the get method is ordinary or not added

---
+++ autocomment_netbeans/spooned/testGet.java
@@ -0,0 +1,24 @@
+
+
+// default package (CtPackage.TOP_LEVEL_PACKAGE_NAME in Spoon= unnamed package)
+
+
+
+public class testGet {
+    private int count;
+
+    private double totalGrade;
+
+    public double getAverage() {
+        return (totalGrade) / (count);
+    }
+
+    public double getTotalGrade() {
+        return totalGrade;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
--- autocomment_netbeans/src/main/java/com/nerzid/autocomment/processor/TrainerMethodProcessor.java
+++ autocomment_netbeans/src/main/java/com/nerzid/autocomment/processor/TrainerMethodProcessor.java
@@ -15,9 +15,17 @@
  */
 package com.nerzid.autocomment.processor;
 
+import com.nerzid.autocomment.nlp.Tokenizer;
 import com.nerzid.autocomment.train.Trainer;
+import java.util.List;
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtCFlowBreak;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtReturn;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
+import spoon.reflect.visitor.filter.ReturnOrThrowFilter;
 
 /**
  *
@@ -33,16 +41,93 @@ public class TrainerMethodProcessor extends AbstractProcessor<CtMethod> {
 //        String commentStr = "This javadoc is for " + e.getSimpleName();
 //        CtComment c = getFactory().Code().createComment(commentStr, CtComment.CommentType.JAVADOC);
 //        e.addComment(c);
-
-        //        System.out.println("Method Name: " + e.getSimpleName());
+//        System.out.println("Method Name: " + e.getSimpleName());
 //        System.out.println("Comments: " + e.getComments());
 //        System.out.println("Body: " + e.toString());
-        Trainer.train(e.getSimpleName(), e.getType().toString());
+
+        // Get method's simple name without any package extensions
+        String method_name = e.getSimpleName();
+
+        if (isOrdinaryGetMethod(e)) {
+            System.out.println("This is ordinary");
+        } else {
+            System.out.println("NOT ORDINARY!");
+        }
+
+        // This part is to ignore get/set methods.
+        // Checkout related issue #3 on github.com/nerzid/autocomment for further info.
+        // Train database using method's name and return type
+        //Trainer.train(method_name, e.getType().toString());
     }
 
     @Override
     public boolean isToBeProcessed(CtMethod candidate) {
         return true;
+    }
+
+    /**
+     * Checks whether the method is an ordinary get method or not.
+     * Ordinary get method means that it only returns a class field (variable),
+     * and doesn't do any operation on it.
+     * 
+     * e.g. ordinary get method -> return count;
+     * e.g. not ordinary get method -> return count/2;
+     * 
+     * @param e 
+     * @return True if get method is ordinary, false if not.
+     */
+    public boolean isOrdinaryGetMethod(CtMethod e) {
+        // Get method's simple name
+        String method_name = e.getSimpleName();
+
+        // Get method's parent which is class itself
+        CtClass clazz = (CtClass) e.getParent();
+
+        // Get Flow Breakers' list. There are 2 Flow Breakers;
+        // return statements and exceptions throws.
+        List<CtCFlowBreak> flowBreakersList = e.getBody().getElements(new ReturnOrThrowFilter());
+
+        // First word must be "get" in method_name
+        if (Tokenizer.split(method_name).get(0).equals("get")) {
+            // Get methods we want to ignore have only 1 return statement
+            // and that return statement is to return one variable without any operation on it
+            if (flowBreakersList.size() == 1) {
+                if (flowBreakersList.get(0) instanceof CtReturn) {
+                    CtReturn returnStmt = (CtReturn) flowBreakersList.get(0);
+                    CtExpression returnExp = returnStmt.getReturnedExpression();
+
+                    // in returnExp, returned variable has always brackets like
+                    // e.g. (variable_name)
+                    String returned_var = returnExp.toString();                   
+                    String[] returnStmtTokens = returnStmt.toString().split(" ");
+                    
+                    // e.g. return count
+                    // first token is always "return" second one always "variable_name"
+                    if (returnStmtTokens.length != 2) {
+                        return false;
+                    } else {
+                        String returnStmtStr = returnStmtTokens[1];
+                        return returned_var.equals(returnStmtStr);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public boolean isOrdinarySetMethod(CtMethod e) {
+        // Get method's simple name
+        String method_name = e.getSimpleName();
+
+        // Get method's parent which is class itself
+        CtClass clazz = (CtClass) e.getParent();
+
+        return false;
     }
 
 }
