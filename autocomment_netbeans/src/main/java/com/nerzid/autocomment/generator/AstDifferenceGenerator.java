2ae777407dc0b5d4551ab1f394ae5611a1a06895
nerzid <nerzid@gmail.com>
2016-10-13 00:11:43 +0300

    Javadoc added for WordGroup, added licence to all

--- autocomment_netbeans/src/main/java/com/nerzid/autocomment/generator/AstDifferenceGenerator.java
+++ autocomment_netbeans/src/main/java/com/nerzid/autocomment/generator/AstDifferenceGenerator.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2016 nerzid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.nerzid.autocomment.generator;
 
@@ -11,7 +21,6 @@ import com.github.gumtreediff.actions.model.Insert;
 import com.github.gumtreediff.actions.model.Move;
 import com.github.gumtreediff.actions.model.Update;
 import com.github.gumtreediff.tree.ITree;
-import com.github.gumtreediff.tree.TreeContext;
 import gumtree.spoon.AstComparator;
 import gumtree.spoon.builder.SpoonGumTreeBuilder;
 import gumtree.spoon.diff.Diff;
@@ -19,7 +28,6 @@ import gumtree.spoon.diff.operations.Operation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.Scanner;
 import spoon.compiler.SpoonCompiler;
 import spoon.compiler.SpoonResourceHelper;
 import spoon.reflect.declaration.CtType;
--- autocomment_netbeans/src/main/java/com/nerzid/autocomment/nlp/NLPToolkit.java
+++ autocomment_netbeans/src/main/java/com/nerzid/autocomment/nlp/NLPToolkit.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2016 nerzid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.nerzid.autocomment.nlp;
 
@@ -17,6 +27,15 @@ import java.util.List;
  * @author nerzid
  */
 public class NLPToolkit {
+    
+    /**
+     * 
+     * @param identifiers_sentence
+     * @param data_type
+     * @return
+     * @deprecated Use {@link #getWordGroup(java.lang.String, java.lang.String) getWordGroup} instead
+     */
+    @Deprecated
     public static Collection<Word> getWordsWithFeatures(String identifiers_sentence, String data_type){
         Collection<Word> words_list = new ArrayList<>();
         String[] identifiers_list = identifiers_sentence.split(" ");
@@ -35,14 +54,20 @@ public class NLPToolkit {
         return words_list;
     }
     
+    /**
+     * Gets the {@link com.nerzid.autocomment.database.WordGroup} instance.
+     * 
+     * @param identifiers_sentence Use {@link com.nerzid.autocomment.nlp.Tokenizer#getIdentifiersSentence(java.util.Collection) getIdentifiersSentence}
+     * @param data_type Use {@link com.nerzid.autocomment.nlp.Tokenizer#simplifyDataType(java.lang.String) simplifyDataType}
+     * @return 
+     */
     public static WordGroup getWordGroup(String identifiers_sentence, String data_type){
         Collection<Word> words_list = new ArrayList<>();
         String[] identifiers_list = identifiers_sentence.split(" ");
         Sentence sent = new Sentence(identifiers_sentence);
         List<String> lemmas_list = sent.lemmas();
         List<String> posttags_list = sent.posTags();
-        
-        
+ 
         for(int i = 0; i < lemmas_list.size(); i++){
             words_list.add(new Word(identifiers_list[i],
                     lemmas_list.get(i),
